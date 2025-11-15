a7ebfb6a4a071ec96df67e120f16fddef504ee8b
github-action-benchmark <github@users.noreply.github.com>
2025-11-15 03:11:41 +0000

    add Benchmark (go) benchmark result for c9adcf81ec0161e62bcc585c78f617a8333b0eda

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763175980895,
+  "lastUpdate": 1763176301597,
   "repoUrl": "https://github.com/ClickHouse/ClickBOM",
   "entries": {
     "Benchmark": [
@@ -1248,6 +1248,54 @@ window.BENCHMARK_DATA = {
             "value": 14,
             "unit": "allocs/op",
             "extra": "122769 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "julio@clickhouse.com",
+            "name": "Julio Jimenez",
+            "username": "juliojimenez"
+          },
+          "committer": {
+            "email": "julio@clickhouse.com",
+            "name": "Julio Jimenez",
+            "username": "juliojimenez"
+          },
+          "distinct": true,
+          "id": "c9adcf81ec0161e62bcc585c78f617a8333b0eda",
+          "message": "fix: maxDepthLevel\n\nSigned-off-by: Julio Jimenez <julio@clickhouse.com>",
+          "timestamp": "2025-11-14T22:10:41-05:00",
+          "tree_id": "19ad7af9c245e238f22396183e9c86f06d87b216",
+          "url": "https://github.com/ClickHouse/ClickBOM/commit/c9adcf81ec0161e62bcc585c78f617a8333b0eda"
+        },
+        "date": 1763176300701,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDetectSBOMFormat",
+            "value": 9426,
+            "unit": "ns/op\t    1152 B/op\t      14 allocs/op",
+            "extra": "124513 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - ns/op",
+            "value": 9426,
+            "unit": "ns/op",
+            "extra": "124513 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - B/op",
+            "value": 1152,
+            "unit": "B/op",
+            "extra": "124513 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - allocs/op",
+            "value": 14,
+            "unit": "allocs/op",
+            "extra": "124513 times\n4 procs"
           }
         ]
       }

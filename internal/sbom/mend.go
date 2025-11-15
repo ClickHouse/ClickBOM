fe1fe95b8a231064a6fbf3eef2c14e332220dc78
Julio Jimenez <julio@clickhouse.com>
2025-11-14 21:51:30 -0500

    fix: maxDepthLevel
    Signed-off-by: Julio Jimenez <julio@clickhouse.com>

--- internal/sbom/mend.go
+++ internal/sbom/mend.go
@@ -170,7 +170,7 @@ func (m *MendClient) RequestSBOMExport(ctx context.Context, outputFile string) e
 		"reportType":             "cycloneDX_1_5",
 		"format":                 "json",
 		"includeVulnerabilities": false,
-		"maxDepthLevel":          100,
+		"maxDepthLevel":          10,
 	}
 
 	// Add scope

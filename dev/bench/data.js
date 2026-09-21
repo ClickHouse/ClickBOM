6bf7796cb097c79d0daea33b68b3bd38c6a29c95
github-action-benchmark <github@users.noreply.github.com>
2026-09-21 11:05:24 +0000

    add Benchmark (go) benchmark result for a5f21d916d7fab5ec8733eb23b65456219a42f8d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789988706920,
+  "lastUpdate": 1789988724606,
   "repoUrl": "https://github.com/ClickHouse/ClickBOM",
   "entries": {
     "Benchmark": [
@@ -6816,6 +6816,54 @@ window.BENCHMARK_DATA = {
             "value": 14,
             "unit": "allocs/op",
             "extra": "106058 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a5f21d916d7fab5ec8733eb23b65456219a42f8d",
+          "message": "Bump actions/checkout from 6 to 7 in /.github/workflows\n\nBumps [actions/checkout](https://github.com/actions/checkout) from 6 to 7.\n- [Release notes](https://github.com/actions/checkout/releases)\n- [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/actions/checkout/compare/v6...v7)\n\n---\nupdated-dependencies:\n- dependency-name: actions/checkout\n  dependency-version: '7'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2026-09-21T11:04:42Z",
+          "tree_id": "d1999f5217e43eee947c0561be030bae7783ed3f",
+          "url": "https://github.com/ClickHouse/ClickBOM/commit/a5f21d916d7fab5ec8733eb23b65456219a42f8d"
+        },
+        "date": 1789988723553,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDetectSBOMFormat",
+            "value": 9264,
+            "unit": "ns/op\t    1152 B/op\t      14 allocs/op",
+            "extra": "129327 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - ns/op",
+            "value": 9264,
+            "unit": "ns/op",
+            "extra": "129327 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - B/op",
+            "value": 1152,
+            "unit": "B/op",
+            "extra": "129327 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectSBOMFormat - allocs/op",
+            "value": 14,
+            "unit": "allocs/op",
+            "extra": "129327 times\n4 procs"
           }
         ]
       }
